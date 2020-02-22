a7bfe50866613b9b99948934f33a9ed2b8aee8e3
Sameer DCosta <sameerdcosta@gmail.com>
2020-02-21 20:54:21 -0600

    Using a variable to switch between GPU and CPU

--- chtc/pytorch_reweighting.sh
+++ chtc/pytorch_reweighting.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Shell script to launch reweighting script
+# $1 is the postfix to the output tar.gz file
+# $2 onwards are passed onto the python program
 
 # set up the staging environment
 tar -zxvf staging.tar.gz
 # create the output directory where we can store stuff to return
 mkdir output
 
-# pass all arguments to the python program
+# pass all arguments (except the first one) to the python program
 cd source
-python reweighting.py "$@"
+python reweighting.py "${@:2}"
 
 # tar up the output directory
 cd ..
-tar -zcvf output.tar.gz -C output/ .
+tar -zcvf output_"$1".tar.gz -C output/ .
 
 # clean up all subdirectories
 rm -rf */
