8bc94dacc8261dce65a73619fd7d1483d50ca2d9
James Wang <jlwang5@wisc.edu>
2020-03-04 14:04:48 -0600

    hopefully fixed memory leak, also switched tabs to spaces

--- source/vae.py
+++ source/vae.py
@@ -10,90 +10,92 @@ import config
 from model import VAE
 from dataloader import MSADataset, OneHotTransform
 
+#TODO: use spyder, check memory usage of loss arrays
+
 def train_model(device, model, trainloader, valloader, max_epochs, convergence_limit, learning_rate):
-	start_time = time.time()
-	min_loss = 999999
-	no_improvement = 0
-	train_loss = []
-	val_loss = []
-	optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-	for epoch in range(max_epochs):
-		if not epoch%100:
-			print('Epoch: %i\tTime elapsed:%.2f sec'%(epoch, (time.time()-start_time)))
-		train_loss.append([])
-		val_loss.append([])
+    start_time = time.time()
+    min_loss = 999999
+    no_improvement = 0
+    train_loss = []
+    val_loss = []
+    optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    for epoch in range(max_epochs):
+        if not epoch%100:
+            print('Epoch: %i\tTime elapsed:%.2f sec'%(epoch, (time.time()-start_time)))
 
-		if no_improvement > convergence_limit:
-			print("convergence at %i iterations" % epoch)
-		for batch_id, (input_images, weights) in enumerate(trainloader):
-			input_images = input_images.to(device)
-			weights = weights.to(device)
+        loss_sum = 0
+        if no_improvement > convergence_limit:
+            print("convergence at %i iterations" % epoch)
+        for batch_id, (input_images, weights) in enumerate(trainloader):
+            input_images = input_images.to(device)
+            weights = weights.to(device)
 
-			z_mean, z_log_var, encoded, recon_images = model(input_images)
-			kld = utils.kl_divergence(z_mean, z_log_var)
-			bce = utils.bce(recon_images, input_images, weights)
-			loss = kld+bce
+            z_mean, z_log_var, encoded, recon_images = model(input_images)
+            kld = utils.kl_divergence(z_mean, z_log_var)
+            bce = utils.bce(recon_images, input_images, weights)
+            loss = kld+bce
 
-			optimizer.zero_grad()
-			loss.backward
-			optimizer.step()
+            optimizer.zero_grad()
+            loss.backward
+            optimizer.step()
 
-			train_loss[epoch].append(loss)
+            loss_sum += torch.sum(loss.detach())
+        train_loss.append(loss_sum/len(trainloader))
 
-		#validation
-		with torch.no_grad():
-			loss = 0
-			for batch_id, (input_images, weights) in enumerate(valloader):
-				input_images = input_images.to(device)
-				weights = weights.to(device)
+        #validation
+        with torch.no_grad():
+            loss_sum = 0
+            for batch_id, (input_images, weights) in enumerate(valloader):
+                input_images = input_images.to(device)
+                weights = weights.to(device)
 
-				z_mean, z_log_var, encoded, recon_images = model(input_images)
-				kld = utils.kl_divergence(z_mean, z_log_var)
-				bce = utils.bce(recon_images, input_images, weights)
-				loss += kld+bce
-			loss=loss/len(valloader)
-			if min_loss < loss:
-				no_improvement += 1
-			else:
-				min_loss = loss
-				no_improvement = 0
+                z_mean, z_log_var, encoded, recon_images = model(input_images)
+                kld = utils.kl_divergence(z_mean, z_log_var)
+                bce = utils.bce(recon_images, input_images, weights)
+                loss_sum += torch.sum(kld+bce)
+            loss=loss_sum/len(valloader)
+            if min_loss < loss:
+                no_improvement += 1
+            else:
+                min_loss = loss
+                no_improvement = 0
+                val_loss[epoch].append(loss)
 
-				val_loss[epoch].append(loss)
-	torch.save(model.state_dict(), "model.pt")
-	pickle.dump({'validation loss': val_loss, 'train_loss':train_loss}, open('loss.pkl', 'wb'))
+    torch.save(model.state_dict(), "model.pt")
+    pickle.dump({'validation loss': val_loss, 'train_loss':train_loss}, open('loss.pkl', 'wb'))
 
 def main():
 
-	input_length = config.input_length
-	num_hidden = config.num_hidden
-	num_latent = config.num_latent
-	activation_func = config.activation_func
-	learning_rate = config.learning_rate
-	device = config.device
-	model = VAE(input_length, num_hidden, num_latent, activation_func, device)
+    input_length = config.input_length
+    num_hidden = config.num_hidden
+    num_latent = config.num_latent
+    activation_func = config.activation_func
+    learning_rate = config.learning_rate
+    device = config.device
+    model = VAE(input_length, num_hidden, num_latent, activation_func, device)
 
 
-	if os.path.exists(config.prev_model):
-		print("Loading saved model...")
-		model.load_state_dict(torch.load(config.prev_model))
+    if os.path.exists(config.prev_model):
+        print("Loading saved model...")
+        model.load_state_dict(torch.load(config.prev_model))
 
-	model.to(device)
+    model.to(device)
 
-	dataset = MSADataset(config.msa, transform=OneHotTransform(21))
-	trainsize = int(len(dataset)*.9)
-	valsize = len(dataset) - trainsize
-	(trainset, valset) = torch.utils.data.random_split(dataset, (trainsize, valsize))
-
-
-	batch_size = config.batch_size
-	trainloader = DataLoader(dataset=trainset, batch_size=batch_size)
-	valloader = DataLoader(dataset=valset, batch_size=batch_size)
+    dataset = MSADataset(config.msa, transform=OneHotTransform(21))
+    trainsize = int(len(dataset)*.9)
+    valsize = len(dataset) - trainsize
+    (trainset, valset) = torch.utils.data.random_split(dataset, (trainsize, valsize))
 
 
+    batch_size = config.batch_size
+    trainloader = DataLoader(dataset=trainset, batch_size=batch_size)
+    valloader = DataLoader(dataset=valset, batch_size=batch_size)
 
-	max_epochs = config.max_epochs
-	convergence_limit = config.convergence_limit
-	train_model(device, model, trainloader, valloader, max_epochs, convergence_limit, learning_rate)
+
+
+    max_epochs = config.max_epochs
+    convergence_limit = config.convergence_limit
+    train_model(device, model, trainloader, valloader, max_epochs, convergence_limit, learning_rate)
 
 if __name__=='__main__':
-	main()
+    main()
