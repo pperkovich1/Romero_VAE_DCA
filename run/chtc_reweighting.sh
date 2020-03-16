f79767da84c35cae116da9b7111cedfb2290d9d1
Sameer D'Costa <sameerdcosta@gmail.com>
2020-03-16 11:56:52 -0500

    Making sure that the chtc script is only run on the group server

--- run/chtc_reweighting.sh
+++ run/chtc_reweighting.sh
@@ -4,6 +4,17 @@
 # $1 is the postfix to the output tar.gz file
 # $2 onwards are passed onto the python program
 
+
+# Group server check!!
+# if the home directory has a @ in it then we
+# might be on the group server so do not execute
+# as this script will delete all subdirectories in 
+# the parent directory without asking
+if [[ $HOME == *"@"* ]]; then 
+  echo "This script is only meant to be executed on chtc. Is this the group server?"; 
+  exit 1
+fi
+
 # set up the staging environment
 tar -zxvf staging.tar.gz
 # create the output directory where we can store stuff to return
--- run/reweighting.sh
+++ run/reweighting.sh
@@ -2,28 +2,5 @@
 
 # Shell script to launch reweighting script
 
-
-# Read in the dataset name and threshold
-# We pick out the first line that doesn't start with a #
-DATASET="dataset.txt"
-IFS="," read -r inputfasta threshold \
-        < <( cat "${DATASET}" | grep -v "^#" | head -1)
-
-echo "Input Fasta =" $inputfasta
-echo "Threshold =" $threshold
-
 cd source
-python reweighting.py 
-
-
-## pass all arguments (except the first one) to the python program
-#cd source
-#python reweighting.py "${@:2}"
-#
-## tar up the output directory
-#cd ..
-#tar -zcvf output_"$1".tar.gz -C output/ .
-#
-## clean up all subdirectories
-#rm -rf */
-## output.tar.gz should be returned automatically
+python reweighting.py ../config.yaml
