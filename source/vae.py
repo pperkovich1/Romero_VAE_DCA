48fbf88f63ab82fe93696910dcae046123687c65
James Wang <jlwang5@wisc.edu>
2020-03-25 10:13:53 -0400

    trains on whole set now

--- source/vae.py
+++ source/vae.py
@@ -13,22 +13,22 @@ import utils
 from model import VAE
 from dataloader import MSADataset, OneHotTransform
 
-def train_model(device, model, trainloader, valloader, max_epochs, convergence_limit, learning_rate):
+def train_model(device, model, loader, max_epochs, convergence_limit, learning_rate):
         start_time = time.time()
         min_loss = 999999
         no_improvement = 0
-        train_loss = []
+        loss_history = []
         optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
         for epoch in range(max_epochs):
                 print('Max  memory usage:%d'%(resource.getrusage(resource.RUSAGE_SELF).ru_maxrss))
                 if not epoch%1:
                         print('Epoch: %i\tTime elapsed:%.2f sec'%(epoch, (time.time()-start_time)))
-                train_loss.append(0)
+                loss_history.append(0)
 
                 if no_improvement > convergence_limit:
                         print("convergence at %i iterations" % epoch)
 
-                for batch_id, (input_images, weights) in enumerate(trainloader):
+                for batch_id, (input_images, weights) in enumerate(loader):
                         input_images = input_images.to(device)
                         weights = weights.to(device)
 
@@ -42,16 +42,16 @@ def train_model(device, model, trainloader, valloader, max_epochs, convergence_l
                         optimizer.step()
 
                         loss=loss.detach()
-                        train_loss[epoch] +=loss
-                train_loss[epoch] = train_loss[epoch]/len(trainloader)
-                if train_loss[epoch] < min_loss:
-                    min_loss = train_loss[epoch]
+                        loss_history[epoch] +=loss
+                loss_history[epoch] = loss_history[epoch]/len(loader)
+                if loss_history[epoch] < min_loss:
+                    min_loss = loss_history[epoch]
                     no_improvement = 0
                 else:
                     no_improvement +=1 
 
         torch.save(model.state_dict(), "model.pt")
-        pickle.dump({'loss':train_loss}, open('loss.pkl', 'wb'))
+        pickle.dump({'loss':loss_history}, open('loss.pkl', 'wb'))
 
 def main():
 
@@ -71,20 +71,15 @@ def main():
         model.to(device)
 
         dataset = MSADataset(config.msa, transform=OneHotTransform(21))
-        trainsize = int(len(dataset)*.9)
-        valsize = len(dataset) - trainsize
-        (trainset, valset) = torch.utils.data.random_split(dataset, (trainsize, valsize))
-
 
         batch_size = config.batch_size
-        trainloader = DataLoader(dataset=trainset, batch_size=batch_size)
-        valloader = DataLoader(dataset=valset, batch_size=batch_size)
+        loader = DataLoader(dataset=dataset, batch_size=batch_size)
 
 
 
         max_epochs = config.max_epochs
         convergence_limit = config.convergence_limit
-        train_model(device, model, trainloader, valloader, max_epochs, convergence_limit, learning_rate)
+        train_model(device, model, loader, max_epochs, convergence_limit, learning_rate)
 
 if __name__=='__main__':
         main()
