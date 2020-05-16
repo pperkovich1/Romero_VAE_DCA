260a4cdbf2bb6e45b26829a495923a2a937d445b
Sameer D'Costa <sameerdcosta@gmail.com>
2020-05-15 21:59:42 -0500

    Adding more functionality to MSAwt class

--- source/msa_tools.py
+++ source/msa_tools.py
@@ -1,25 +1,42 @@
 """Manipulate MSAs (stats, extract parts etc)
 
   Typical usage example: (from the command line)
-	#python msa_tools.py \
-	#	-i ../sequence_sets/cmx_aligned_blank_90.fasta \
-	#	-t 0.8 \
-	#	-o ../working/cmx_aligned_blank_90_weights.npy
-        python msa_tools.py -i ../sequence_sets/cmx_aligned_blank_90.fasta \
-                -w ../sequence_sets/cmx_wildtype.fasta                                                                          
-    Distance from WT: 62.36%
-    Sampling 100k pairs to calculate pairwise distance
-    Avg pairwise distance: 63.10%
-    Time elapsed: 0.13 min
-
+	#To get stats printed out to the terminal
+        > python msa_tools.py \
+                -i ../sequence_sets/cmx_aligned_blank_90.fasta \
+                -w ../sequence_sets/cmx_wildtype.fasta  \
+                -s ''
+        Sampling 100k pairs to calculate pairwise distance
+        avg_dist_from_wt_pct: 62.36%
+        avg_pairwise_dist_pct: 63.12%
+        name: cmx_aligned_blank_90
+        num_seqs: 14441
+        seq_length: 559
+        Time elapsed: 0.11 min
 """
 
+import sys
 import functools
+from contextlib import ExitStack
+import gzip
+
+import pathlib
 import numpy as np
+
+import yaml
 
 import dataloader
 
 class MSA:
+    """ Perform manipulations on MSA 
+
+    TODO: Move functions that do not use WT into here
+    """
+
+    pass
+
+
+class MSAwt(MSA):
     """ Perform manipulations on MSA 
 
     Store an MSA as a numpy "|S1" array (most efficient form)
@@ -27,14 +44,16 @@ class MSA:
 
     """
 
-    def __init__(self, msa, wt):
+    def __init__(self, msa, wt, name=""):
         """
         Args:
             msa    : Numpy array of shape (N, L)  dtype "|S1"
             wt     : Numpy array of shape (1, L) or (L,)  dtype "|S1"
+            name   : A string name for the MSA
         """
         self.msa = msa
         self.wt = wt
+        self.name = name
         wt = wt.squeeze()
         if len(wt.shape) != 1:
             raise ValueError("WildType filename must have only one value in it")
@@ -61,7 +80,7 @@ class MSA:
         return (self.msa != seq).sum(axis=1).mean()
 
     @property
-    @functools.lru_cache(1)
+    @functools.lru_cache(1) # save last calculation. Can use @cachedproperty for python 3.8+
     def avg_pairwise_dist(self):
         """ This is not a property because it takes forever to calculate """
         if self.num_seqs > 1000:
@@ -79,11 +98,54 @@ class MSA:
     def avg_pairwise_dist_pct(self):
         return self.avg_pairwise_dist / self.seq_length * 100
 
+    def get_stats_dict(self):
+        return {'name': self.name,
+                'num_seqs': self.num_seqs,
+                'seq_length':self.seq_length,
+                'avg_dist_from_wt_pct':f"{self.avg_dist_from_wt_pct:.2f}%",
+                'avg_pairwise_dist_pct':f"{self.avg_pairwise_dist_pct:.2f}%"}
+
+    def write_stats_dict(self, filetype):
+        """ Write stats dictionary out to a file
+
+            Args:
+                filetype : str or filehandle (even something like sys.stdout)
+        """
+        with ExitStack() as stack: # make sure any file handles are closed if opened
+            fh = sys.stdout
+            if isinstance(filetype, str):
+                if filetype:
+                    # Let's open a filehandle
+                    p = pathlib.Path(filetype)
+                    fh = stack.enter_context(p.open('wt'))
+            else:
+                fh = filetype # try printing directly to filetype
+                # no need to close fh now as whatever passed it in will take care of it
+            yaml.dump(self.get_stats_dict(), fh)
+
+    def remove_seqs_below_mean(self):
+        """ Return a new MSA with seqs below mean dist from WT removed """
+        dist_from_wt = (self.msa != self.wt).sum(axis=1)
+        # make a shallow copy
+        return MSAwt(msa=self.msa[dist_from_wt < self.avg_dist_from_wt, :],
+                    wt = self.wt,
+                    name = self.name)
+        
+    def write_msa_to_file(self, filename):
+        """ Only supported writing gzip aln files for now"""
+        filename = pathlib.Path(filename).with_suffix(".gz")
+        with gzip.open(filename, "wt") as fh:
+            for m in self.msa: 
+                fh.write(m.tostring().decode("ascii")) 
+                fh.write("\n") 
+
+
     @staticmethod
     def create_from_files(msa_filename, wildtype_filename):
-        msa = dataloader.get_msa_from_file(args.msa_filename, as_numpy=True)
-        wt = dataloader.get_msa_from_file(args.wildtype_filename, as_numpy=True)
-        return MSA(msa, wt)
+        msa = dataloader.get_msa_from_file(msa_filename, as_numpy=True)
+        wt = dataloader.get_msa_from_file(wildtype_filename, as_numpy=True)
+        name = pathlib.Path(msa_filename).stem.split('.')[0]
+        return MSAwt(msa, wt, name)
 
 
 if __name__ == "__main__":
@@ -97,14 +159,26 @@ if __name__ == "__main__":
     parser.add_argument("-w", "--wildtype_filename",
                     help="filename that contains only WildType",
                     required=True) # required for now! TODO: make optional later
+    parser.add_argument("-s", "--stats_filename", 
+                    default=None, help="filename to save stats to")
+    parser.add_argument("-r", "--remove_below_mean_filename", 
+                    help="filename to save new MSA with seqeuences below mean removed")
+    parser.add_argument("-f", "--remove_below_mean_yaml", 
+                    help="filename to save stats to")
     args = parser.parse_args()
 
     start_time = time.time()
 
-    msa = MSA.create_from_files(args.msa_filename, args.wildtype_filename)
+    msa = MSAwt.create_from_files(args.msa_filename, args.wildtype_filename)
 
-    print(f"Distance from WT: {msa.avg_dist_from_wt_pct:.2f}%")
-    print(f"Avg pairwise distance: {msa.avg_pairwise_dist_pct:.2f}%")
+    if args.stats_filename is not None: # can be an empty string
+        msa.write_stats_dict(args.stats_filename)
+
+    if args.remove_below_mean_filename:
+        msa_above_mean = msa.remove_seqs_below_mean()
+        msa_above_mean.write_msa_to_file(args.remove_below_mean_filename)
+
+
 
     print('Time elapsed: %.2f min' % ((time.time() - start_time)/60))
 
