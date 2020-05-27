dba3e85e9e1da2885479cd074fc1de4bf12a21ad
jlwang5 <jlwang5@submit2.chtc.wisc.edu>
2020-05-27 09:54:44 -0500

    chtc now iterates through list of configs

--- run/chtc_train_model.sh
+++ run/chtc_train_model.sh
@@ -3,10 +3,11 @@
 # Shell script to launch reweighting script
 Cluster=$1
 Process=$2
-dataset=$3
-hidden=$4
-latent=$5
-config=$6
+config=$3
+# dataset=$3
+# hidden=$4
+# latent=$5
+# config=$6
 
 TOPDIR_FILE=chtc_root.txt
 if [ -f "$TOPDIR_FILE" ]; then
@@ -19,34 +20,32 @@ fi
 
 # set up the staging environment
 tar -zxf staging.tar.gz
+tar -zxf sequences.tar.gz
 # create the output directory where we can store stuff to return
 mkdir output
 
-cp config.yaml output
+cp "$config" output
 
 # TODO: make editing config file less janky
 # TODO: it'd be nice if I could replace ../config.yaml with $config
 # python modify_config.py $(Process) $(config)
 # add dataset to config file
-printf "\naligned_msa_filename: %s\n" $dataset >> config.yaml
-printf "\nhidden_layer_size: %s\n" $hidden >> config.yaml
-printf "\nlatent_layer_size: %s\n" $latent >> config.yaml
-cat config.yaml
+# printf "\naligned_msa_filename: %s\n" $dataset >> config.yaml
+# printf "\nhidden_layer_size: %s\n" $hidden >> config.yaml
+# printf "\nlatent_layer_size: %s\n" $latent >> config.yaml
+# cat config.yaml
  
-# move dataset into sequence_sets folder
-cp $dataset sequence_sets
-
 # for debuging purposes
 ls -aR
 
 # just in case our sh files are not executable
-chmod +x *.sh
+chmod +x run/*.sh
 
 # pass config file location to the python program
-./runmodel.sh $config
+run/runmodel.sh $config
 
 # tar up the output directory
-tar -zcf training_output_"$hidden"_"$latent".tar.gz ./output 
+tar -zcf training_output_"$Process".tar.gz ./output 
 
 # clean up all subdirectories
 if [ -f "$TOPDIR_FILE" ]; then
