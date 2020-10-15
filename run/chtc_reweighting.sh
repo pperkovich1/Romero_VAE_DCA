edd204d23a9bc8cc3430f1541f393ceef66e270c
Sameer D'Costa <sameerdcosta@gmail.com>
2020-10-15 08:08:04 -0500

    Removing chtc_root since we do everything in a subdirectory

--- run/chtc_reweighting.sh
+++ run/chtc_reweighting.sh
@@ -4,14 +4,15 @@
 # $1 is the postfix to the output tar.gz file
 # $2 onwards are passed onto the python program
 
-TOPDIR_FILE=chtc_root.txt
-if [ -f "$TOPDIR_FILE" ]; then
-    echo "$TOPDIR_FILE exist"
-else
-    echo "$TOPDIR_FILE does not exist"
-    echo "This file is needed so that we do not accidentally delete directories "
-    exit 1
-fi
+CHTC_WORK_DIR="chtc_work"
+
+# This is where we will extract all the sequences and source code
+# Subdirectories in chtc are not returned so we do not need to delete
+# anything in this directory after we are done. 
+mkdir -p "${CHTC_WORK_DIR}"
+
+mv config.yaml staging.tar.gz sequences.tar.gz "${CHTC_WORK_DIR}"
+cd "${CHTC_WORK_DIR}"
 
 # set up the staging environment
 tar -zxvf staging.tar.gz
@@ -37,16 +38,10 @@ chmod +x *.sh
 # pass all arguments (except the first one) to the python program
 ./run/reweighting.sh "${@:2}"
 
+
+MODEL_NAME=`python source/read_config.py config.yaml --model_name`
 # tar up the output directory
-tar -zcvf reweighting_output_"$1".tar.gz -C ${WORKINGDIR_NOPARENT}/ .
+OUTPUT_TAR_GZ=reweighting_output_"${MODEL_NAME}".tar.gz
+tar -zcvf ../"${OUTPUT_TAR_GZ}  -C ${WORKINGDIR_NOPARENT}/ .
 
-# clean up all subdirectories
-if [ -f "$TOPDIR_FILE" ]; then
-    rm -rf */ # safely delete sub directories
-    rm -f config.yaml 
-else
-    echo "$TOPDIR_FILE does not exist. "
-    echo "Error: Not cleaning up sub directories"
-fi
-
-# reweighting_output_<msa_filename>.tar.gz should be returned automatically
+# reweighting_output_<model_name>.tar.gz should be returned automatically
