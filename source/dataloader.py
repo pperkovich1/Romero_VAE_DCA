7e9a66860d31b07a3a529477ff3dd9726cc53013
Sameer D'Costa <sameerdcosta@gmail.com>
2021-02-05 08:58:01 -0600

    Refactoring dataloader

--- source/dataloader.py
+++ source/dataloader.py
@@ -27,89 +27,47 @@ codon_map = {c:i for i, c in enumerate(
                     sorted(codon_table.forward_table.keys()))}
 
 
-def get_msa_from_fasta_iter(fasta_filename, size_limit=None):
+def get_msa_from_filename_iter(filename, filetype, size_limit):
     """Reads a fasta file and returns a string iterator  
 
     Args:
-        fasta_filename  : Filename or filehandle of fasta file to read
-
-    """
-    seq_io_gen = SeqIO.parse(fasta_filename, "fasta") # generator of sequences
-    # Read only size_limit elements of the generator
-    # if size_limit is None then we will read in everything
-    seq_io_gen_slice = itertools.islice(seq_io_gen, size_limit) 
-    # Here we can return a generator expression because SeqIO.parse
-    # is handling the file handle
-    return (seq.seq.upper() for seq in seq_io_gen_slice)
- 
-
-def get_msa_from_fasta(fasta_filename, size_limit=None, 
-                            as_numpy=True, as_iter=False):
-    """Reads a fasta file and returns an MSA
-
-    Takes a fasta filename and reads it with SeqIO and converts to a numpy
-    byte array. This function tries to be fast and keep the data in the
-    simplest representation posible. 
-
-    Args:
-        fasta_filename  : Filename or filehandle of fasta file to read
-        size_limit      : Return upto size_limit sequences
-        as_numpy        : return numpy byte array instead of list of seqs
-
-    Returns:
-        if as_iter is True:
-            An iterator of sequences in raw string format
-        if as_numpy is False:
-            A list of sequences in Bio.Seq format
-        if as_numpy is True:
-            A numpy byte array of dtpye S1 which represents the MSA. 
-            The first axis is the sequence number. The second axis is the
-            residue number. 
-    """
-    seqs = get_msa_from_fasta_iter(fasta_filename, size_limit)
-    ret = None
-    if as_iter:
-        ret = seqs
-    elif as_numpy:
-        # convert to lists of lists for easy numpy conversion to 2D array
-        ret = np.array([list(str(s)) for s in seqs], dtype="|S1")
-    else:
-        ret = list(seqs)
-    return ret
-
-def get_msa_from_aln_iter(aln_filename, size_limit):
-    """Reads a (plain text) aln file (can be gzipped also) and iterate
-        over string sequences 
-
-    Args:
-        aln_filename    : Filename or filename of ALN file to read
-        size_limit      : Return upto size_limit sequences
+        filename  : Filename or filehandle of fasta file to read
+        filetype  : "fasta" or "aln"
     """
     opener = open
-    if str(aln_filename).endswith(".gz"):
+    if str(filename).endswith(".gz"):
         opener = gzip.open
-    with opener(aln_filename, "rt") as fh:
-        seq_io_gen = (line.strip() for line in fh)
+    with opener(filename, "rt") as fh:
+        seq_io_gen = None 
+        if filetype == "fasta":
+            seq_io_gen = SeqIO.parse(fh, "fasta") # generator of sequences
+        elif filetype == "aln":
+            seq_io_gen = (line.strip() for line in fh)
+        if seq_io_gen is None:
+            raise ValueError ("filetype can only be fasta or aln")
         # Read only size_limit elements of the generator
         # if size_limit is None then we will read in everything
         seq_io_gen_slice = itertools.islice(seq_io_gen, size_limit) 
-        # We need the yield from statement below so that the file
-        # handle is kept open as long as we are reading from it
-        # returning a generator expression would close fh
-        yield from (seq.upper() for seq in seq_io_gen_slice)
+        # Here we can return a generator expression because SeqIO.parse
+        # is handling the file handle
+        yield from (seq.seq.upper() for seq in seq_io_gen_slice)
+  
 
-def get_msa_from_aln(aln_filename, size_limit=None, 
+def get_msa_from_filename(filename, filetype, size_limit=None, 
                             as_numpy=True, as_iter=False):
-    """Reads a (plain text) aln file (can be gzipped also) and returns an MSA
+    """Reads a (plain text) fasta/aln file (can be gzipped also) and returns an
+        MSA
 
-    Takes a simple text file (ALN) which has one sequence per line. Returns 
+    Takes a simple text file (ALN) which has one sequence per line. OR
+    Takes a fasta file Returns 
     and MSA as a numpy array or as a list of Bio.Seq sequences.
 
     Args:
-        aln_filename    : Filename or filename of ALN file to read
-        size_limit      : Return upto size_limit sequences
-        as_numpy        : return numpy byte array instead of list of seqs
-        as_iter         : return the raw string iterator instead 
+        filename    : Filename or filename of ALN/FASTA file to read
+        filetype    : "fasta" or "aln"
+        size_limit  : Return upto size_limit sequences
+        as_numpy    : return numpy byte array instead of list of seqs
+        as_iter     : return the raw string iterator instead 
 
     Returns:
         if as_iter is True:
@@ -121,7 +79,7 @@ def get_msa_from_aln(aln_filename, size_limit=None,
             The first axis is the sequence number. The second axis is the
             residue number. 
     """
-    seqs = get_msa_from_aln_iter(aln_filename, size_limit)
+    seqs = get_msa_from_filename_iter(filename, filetype, size_limit)
     ret = None
     if as_iter: # return the raw iterator
         ret = seqs
@@ -140,12 +98,13 @@ def get_msa_from_file(msa_file, size_limit=None, as_numpy=True, as_iter=False):
     """
     suffixes = pathlib.Path(str(msa_file)).suffixes
     suffix = suffixes[-1]
+    filetype = None
     if len(suffixes) > 1 and suffixes[-1] == ".gz":
         suffix = suffixes[-2] # handle .fasta.gz
     if suffix == ".fasta" or suffix == ".a2m":
-        file_reader_func = get_msa_from_fasta
+        filetype = "fasta"
     elif suffix == ".txt" or suffix == ".text" or suffix == ".aln":
-        file_reader_func = get_msa_from_aln
+        filetype = "aln"
     else:
         err_str = f"Input MSA must have format FASTA/A2M or TEXT/ALN.\n" \
                   f"The file extension must be one of " \
@@ -153,8 +112,8 @@ def get_msa_from_file(msa_file, size_limit=None, as_numpy=True, as_iter=False):
                   f".fasta.gz/.a2m.gz/.txt.gz/.text.gz/.aln.gz if compressed." \
                   f"Found extension {suffix}"
         raise ValueError(err_str)
-    return file_reader_func(msa_file, size_limit=size_limit, as_numpy=as_numpy,
-            as_iter=as_iter)
+    return get_msa_from_filename(msa_file, filetype, size_limit=size_limit,
+            as_numpy=as_numpy, as_iter=as_iter)
 
 def get_codon_msa_as_int_array(filename, codon_map):
     """
@@ -209,7 +168,7 @@ class MSADataset(Dataset):
     def __len__(self):
         return len(self.raw_data)
 
-    def conver_protein_to_tensor(self, prot_str):
+    def protein_to_tensor(self, prot_str):
         # Integer representation of the protein
         prot_int = [self.AA_enc[p] for p in prot_str]
         sample = torch.LongTensor(prot_int)
@@ -220,7 +179,7 @@ class MSADataset(Dataset):
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
-        sample = (self.conver_protein_to_tensor(self.raw_data[idx]), self.weights[idx])
+        sample = (self.protein_to_tensor(self.raw_data[idx]), self.weights[idx])
         return sample
 
 class OneHotTransform:
