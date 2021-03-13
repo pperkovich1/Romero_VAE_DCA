810c7b7dbdb784e64e2cbf22d32dbc90e52f2141
Sameer D'Costa <sameerdcosta@gmail.com>
2021-03-13 08:24:02 -0600

    Adding a couple more helper scripts

---
+++ bin/read_config.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Run the msa_tools.py python program as a shell script
+
+
+# Activate Conda environment
+conda_source="/home/romeroroot/miniconda3/bin/activate"
+source ${conda_source} pytorch-docker
+
+
+# Find out where this script is located
+# It should be in the bin directory of a VAE installation
+SCRIPT=`realpath $0` 
+SCRIPTPATH=`dirname $SCRIPT`
+
+# Find out the python source directory for the corresponding
+# VAE directory
+SOURCEDIR="$SCRIPTPATH/../source" 
+
+# ADD Source to PYTHONPATH
+PYTHONPATH="${SOURCEDIR}:${PYTHONPATH}" python "${SOURCEDIR}"/read_config.py "$@"
